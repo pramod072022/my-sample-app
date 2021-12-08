547838fa121eff66db3189e10a6e55a70fc8fc27
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2021-12-08 09:11:22 +0530

    Fourt Commit

--- src/main/java/com/devops/app/App.java
+++ src/main/java/com/devops/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from AWS and maven and git !!!!!" );
+        System.out.println( "Hello World from AWS and maven and git " );
     }
 }
